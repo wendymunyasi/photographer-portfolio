2f2aaa15f8746efba505769062b8e9f2517b28ae
wendymunyasi <wendymunyasi@gmail.com>
2020-07-23 22:38:43 +0300

    Add hover effects.

--- src/assets/js/custom-js/custom.js
+++ src/assets/js/custom-js/custom.js
@@ -2,20 +2,10 @@ $(function() {
   var effects = 'animated bounce';
   var effectsEnd = 'animationend onAnimationEnd mozAnimationEnd webkitAnimationEnd';
 
-  $('button.notify').hover(function() {
+  $('img.arrow').hover(function() {
     $(this).addClass(effects).one(effectsEnd, function() {
       $(this).removeClass(effects);
     });
-  });
-});
-
-
-
-$(document).ready(function() {
-  $('.popup').click(function() {
-    var src = $(this).attr('src'); 
-    $('.modal').modal('show');
-    $('#popup-img').attr('src', src)  
   });
 });
 
