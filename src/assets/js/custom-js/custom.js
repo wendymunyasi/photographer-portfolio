aaa7ae84d3218a450c5e805104a0b4ec7db57bed
wendymunyasi <wendymunyasi@gmail.com>
2020-07-24 12:19:16 +0300

    Edit some animations.

--- src/assets/js/custom-js/custom.js
+++ src/assets/js/custom-js/custom.js
@@ -9,3 +9,13 @@ $(function() {
   });
 });
 
+
+window.addEventListener('scroll', function(e) {
+
+  if( $(window).scrollTop() <= 50) {
+      $('.wow').removeClass('animated');
+      $('.wow').removeAttr('style');
+      new WOW().init();
+  }
+
+});
